3dc90435560ff36c2ad89d84841b979b828d8168
tamybl <tamara.bravo.l@gmail.com>
2018-02-15 14:16:07 -0300

    Llamada ajax detalles personajes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
-    var url = 'https://raw.githubusercontent.com/tamybl/swapi-app/master/assets/js/characters.json';
+    let url = 'https://raw.githubusercontent.com/tamybl/swapi-app/master/assets/js/characters.json';
     $.ajax ({
       type: 'GET',
       url: url,
-      success: success,
+      success: getCharacters,
       error: error
     });
 })
 
-function success(characters) {
+function getCharacters(characters) {
   characters = JSON.parse(characters);
-  console.log('success:', characters);
+  //console.log('success:', characters);
   for (var i = 0; i < characters.characters.length; i++) {
-    $('#contenedor').append('<div class="col-xs-4 character"><div class="photo"><img src='+characters.characters[i].img+' alt=""></div><div class="name">Luke Skywalker</div><div class="view"><button class="btn text-uppercase">Info</button></div></div>');
+    $('#contenedor').append('<div class="col-xs-4 character"><div class="photo"><img src='+characters.characters[i].img+' alt=""></div><div class="name">'+characters.characters[i].name+'</div><div class="view"><a href="'+characters.characters[i].id+'" class="btn text-uppercase myModal" data-toggle="modal" data-target="#myModal">Info</a></div></div>');
   }
 
-  
+  $('.myModal').click(function () {
+    var id = $(this).attr('href');
+    let url = 'https://swapi.co/api/people/'+id;
+    console.log(url);
+    $.ajax({
+      url: url,
+      success: getDetails,
+      error: error
+    });
+  })
 }
 
+function getDetails(details) {
+  console.log(details)
+
+}
 
 function error() {
   console.log('Ha ocurrido un error');
